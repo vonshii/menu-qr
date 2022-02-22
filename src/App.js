2333655064f457d7ad350a4fb2983d014f5c0c89
vonshi <germanvscheidt@gmail.com>
2022-02-22 01:24:46 -0300

    se vinculo al repositorio remoto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Al fin uraaaa
+          Menu Qr
         </p>
         <a
           className="App-link"
